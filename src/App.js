252bccd2885d696b68314c638633330d04384306
Vladimir <1337vovalenovo@gmail.com>
2022-11-20 11:38:23 +0300

    second-commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ function App() {
   const [name, setName] = useState('')
   const [lastName, setLastName] = useState('')
 
-  // const handleClick = (event) => {
-  //   console.log(name)
-  //   // console.log(e.target.value)
-  //   if(event.target.id === 'lastname'){
-  //     setLastName(event.currentTarget.value)
-  //   }
-  //   if(event.target.id === 'name'){
-  //     setName(event.currentTarget.value)
-  //   }
-  // }
+  const handleClick = (event) => {
+    console.log(name)
+    // console.log(e.target.value)
+    switch(event.target.id) {
+      case 'lastname':
+        setLastName(event.currentTarget.value)
+        break;
+      case 'name':
+        setName(event.currentTarget.value)
+        break;
+    }
+    // if(event.target.id === 'lastname'){
+    //   setLastName(event.currentTarget.value)
+    // }
+    // if(event.target.id === 'name'){
+    //   setName(event.currentTarget.value)
+    // }
+  }
 
   return (
     <div className="App">
